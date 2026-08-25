6b1841a1c1bb337e2209e25cda132c21485a5362
Miguel Murillo <miguelmurillo@Miguels-MacBook-Pro.local>
2026-08-25 18:46:03 -0500

    fix: modified order of imports in api.py

--- src/intent_parser/api.py
+++ src/intent_parser/api.py
@@ -1,10 +1,10 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
-from dotenv import load_dotenv
 
 from intent_parser.anthropic_client import AnthropicClient
-from intent_parser.parser import parse_intent
 from intent_parser.models import Intent
+from intent_parser.parser import parse_intent
 
 load_dotenv()
 
